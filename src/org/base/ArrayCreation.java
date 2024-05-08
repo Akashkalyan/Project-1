35f0113a3c3139e0ebc81250957e6e0c7b33cbda
git-almed <git@almedtech.com>
2024-05-08 10:54:01 +0530

    Array file

---
+++ src/org/base/ArrayCreation.java
@@ -0,0 +1,82 @@
+package org.base;
+
+public class ArrayCreation {
+	public static void main(String[]args) {
+		
+		
+		String s = "akash is a Best Automation test engineer";
+	    s.replace(" ","");
+		System.out.println(s);
+		
+		for(int i=0;i<s.length();i++) {
+			for(int j=0;j<s.length();j++) {
+					continue;
+				}
+			}
+			
+			
+		}
+        		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+//	     int[] a = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+//	        int sum = 0;
+//
+//	       
+//	        for (int i = 0; i < a.length; i++) {
+//	           sum += a[i];
+//	        }
+//
+//	       
+//	         double average = (double)sum / a.length;
+//
+//	        
+//	        System.out.println("Average = " + average);
+}
+	
+
